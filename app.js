cac3bfe2388297b26e30bc0249311df708e3a5b7
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-03-08 00:33:25 +0900

    divide users route

--- app.js
+++ app.js
@@ -16,32 +16,8 @@ app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/users', require('./routes/users'));
+
 app.use(express.static('public'));
 app.listen(PORT);
-
-
-mongodb.MongoClient.connect('mongodb://localhost:27017/test', function(err, database){
-    users = database.collection('users');
-    // users.insert({name: 'test', date: new Date()});
-});
-
-
-app.get('/users.:format?', function(req, res){
-    // console.log(req.params);
-    users.find().toArray(function(err, users){
-        // res.send(items);
-        res.render('users/index', {
-            users: users
-        });
-    });
-});
-
-app.get('/users/new', function(req, res){
-    res.render('users/new');
-});
-
-app.post('/users/create', function(req, res){
-    // console.log(req.body);
-    users.insert(req.body);
-    res.redirect('/users');
-});
