35c206e82ff667b6b0bf9a2a904c64997f247bd8
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-03-07 23:30:44 +0900

    add users index

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var PORT = 3000;
 var app = express();
 
 // app.set('views', __dirname + '/views');
-// app.set('views engine', 'ejs');
-app.engine('ejs', ejs.renderFile);
+app.set('view engine', 'ejs');
+// app.engine('ejs', ejs.renderFile);
 
 app.use(express.static('public'));
 app.listen(PORT);
@@ -23,11 +23,16 @@ mongodb.MongoClient.connect('mongodb://localhost:27017/test', function(err, data
 
 app.get('/users.:format?', function(req, res){
     // console.log(req.params);
-    users.find().toArray(function(err, items){
-        res.send(items);
+    users.find().toArray(function(err, users){
+        // res.send(items);
+        res.render('users/index', {
+            users: users
+        });
     });
 });
 
 app.get('/users/new', function(req, res){
-    res.render('users/new.ejs');
+    res.render('users/new');
 });
+
+// app.get('/users/')
