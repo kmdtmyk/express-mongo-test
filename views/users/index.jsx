ea67c703bc74a6fbe4d02aacb205f4c128cae949
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-03-10 23:44:54 +0900

    add users/index.jsx

--- views/users/index.jsx
+++ views/users/index.jsx
@@ -12,47 +12,55 @@ module.exports = React.createClass({
                         <i className='search icon'></i>
                     </div>
                 </form>
-                <table>
-                    {this.props.users.length}
-                </table>
+                <UserList users={this.props.users}>
+                </UserList>
+                <a href='/users/new'>new user</a>
             </DefaultLayout>
         );
     }
 });
 
+var UserList = React.createClass({
+    render: function(){
 
+        var tbody = this.props.users.map(function(user){
+            return (
+                <tr>
+                    <td>{user.name}</td>
+                    <td>{user.furigana}</td>
+                    <td>{user.mailAddress}</td>
+                    <td>
+                        <a href={'/users/' + user._id}>show</a>
+                    </td>
+                    <td>
+                        <a href={'/users/' + user._id + '/edit'}>edit</a>
+                    </td>
+                </tr>
+            );
+        });
 
-// block content
-//
-//   h1 users list
-//
-//   form
-//     .ui.search
-//       .ui.icon.input
-//         input.prompt(type='text', name='q', placeholder='search' value='#{query.q}')
-//         i.search.icon
-//
-//   table.ui.celled.table.striped
-//     thead
-//       tr
-//         th name
-//         th furigana
-//         th mail address
-//         th
-//         th
-//     tbody
-//       each user in users
-//         tr
-//           td= user.name
-//           td= user.furigana
-//           td= user.mailAddress
-//           td
-//             a(href='/users/#{user._id}') show
-//           td
-//             a(href='/users/#{user._id}/edit') edit
-//     tfoot
-//       tr
-//         th(colspan='5') Total: #{users.length}
-//
-//
-//   a(href='/users/new') new user
+        return (
+            <table className='ui celled table striped'>
+                <thead>
+                    <tr>
+                        <th>name</th>
+                        <th>furigana</th>
+                        <th>mail address</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tbody}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th colSpan='5'>
+                            Total: {this.props.users.length}
+                        </th>
+                     </tr>
+                </tfoot>
+            </table>
+        );
+    }
+})
