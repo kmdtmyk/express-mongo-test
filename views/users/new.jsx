9994b627f65af58143afeab148a85365ffe32843
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-03-11 22:53:44 +0900

    add new.jsx and edit.jsx

---
+++ views/users/new.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var DefaultLayout = require('../DefaultLayout');
+
+
+module.exports = React.createClass({
+    render: function(){
+        return (
+            <DefaultLayout>
+                <h1>user new</h1>
+                <form className='ui form' action='./' method='post'>
+                    <div class='field'>
+                        <label>name</label>
+                        <input type='text' name='user[name]' value={this.props.user.name} />
+                    </div>
+
+                    <div class='field'>
+                        <label>furigana</label>
+                        <input type='text' name='user[furigana]' value={this.props.user.furigana} />
+                    </div>
+
+                    <div class='field'>
+                        <label>mail address</label>
+                        <input type='text' name='user[mailAddress]' value={this.props.user.mailAddress} />
+                    </div>
+
+                    <button className='ui button' type='submit'>regist</button>
+                </form>
+            </DefaultLayout>
+        );
+    }
+});
