3eb08dfff6fb8de5f8b99f7480ec5414929d34df
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-06-13 19:43:17 +0900

    add search form to user list

--- src/users/Index.vue
+++ src/users/Index.vue
@@ -1,21 +1,31 @@
 <template>
+
   <h1>user list</h1>
-  <a v-link="{path:'new', append: true}">new user</a>
+
+  <form>
+    <input type='search' name='q' value='{{$route.query.q}}'>
+  </form>
+
   <table>
     <thead>
       <tr>
         <th>name</th>
         <th>furigana</th>
+        <th>mail address</th>
       </tr>
     </thead>
     <tbody>
       <tr v-for='user in users'>
         <td>{{user.name}}</td>
         <td>{{user.furigana}}</td>
+        <td>{{user.mailAddress}}</td>
       </tr>
     </tbody>
   </table>
-  <pre>{{$route | json}}</pre>
+
+  <a v-link="{path:'new', append: true}">new user</a>
+
+  <!-- <pre>{{$route | json}}</pre> -->
 </template>
 
 <script>
@@ -29,16 +39,21 @@ export default {
   },
   route: {
     data({ to, next }){
+      // console.log(to)
+      let { query } = to
+      console.log(query)
+
       superagent
         .get('/api/users')
-        .query({ q: 'a' })
+        .query(query)
         .end(function(err, res){
           let users = res.body
           next({users})
         })
 
-    }
-  }
+    },
+  },
+  
 }
 </script>
 
