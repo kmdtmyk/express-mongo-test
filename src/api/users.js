babc597a0ab291a0388b59d8f34ebcf30f463a76
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-06-14 22:19:02 +0900

    add user new

--- src/api/users.js
+++ src/api/users.js
@@ -8,80 +8,85 @@ let User = require('../models/user');
 
 
 router.get('/', (req, res) => {
-    let query = req.query;
-    let regexp = new RegExp(query.q);
+  let query = req.query;
+  let regexp = new RegExp(query.q);
 
-    User
-        .find({
-            $or: [
-                {name: regexp},
-                {furigana: regexp},
-                {mailAddress: regexp},
-            ]
-        })
-        .exec((error, users) => {
-            res.send(users);
-        });
+  User
+    .find({
+      $or: [
+        {name: regexp},
+        {furigana: regexp},
+        {mailAddress: regexp},
+      ]
+    })
+    .exec((error, users) => {
+      res.send(users);
+    });
 
 });
 
 
 
 router.get('/new', (req, res) => {
-    let user = new User();
-    res.send(user);
+  let user = new User();
+  res.send(user);
 });
 
 
 
 router.get('/:id', (req, res) => {
-    let id = req.params.id;
-    User.findOne({_id: id}, (error, user) => {
-        if(error){
-            res.send(error);
-        }else{
-            res.send(user);
-        }
-    });
+  let id = req.params.id;
+
+  User.findOne({_id: id}, (error, user) => {
+    if(error){
+      res.send(error);
+    }else{
+      res.send(user);
+    }
+  });
+
 });
 
 
 
 router.post('/', (req, res) => {
-    let user = req.body;
-    User.create(user, (error, user) => {
-        if(error){
-            res.send(error);
-        }else{
-            res.send(user);
-        }
-    });
+  let user = req.body;
+
+  User.create(user, (error, user) => {
+    if(error){
+      res.send(error);
+    }else{
+      res.send(user);
+    }
+  });
 
 });
 
 
 
 router.patch('/:id', (req, res) => {
-    let id = req.params.id;
-    let user = req.body;
+  let id = req.params.id;
+  let user = req.body;
 
-    User.findOneAndUpdate({_id: id}, {$set: user}, (error, user) => {
-        if(error){
-            res.send(error);
-        }else{
-            res.send(user);
-        }
-    });
+  User.findOneAndUpdate({_id: id}, {$set: user}, (error, user) => {
+    if(error){
+      res.send(error);
+    }else{
+      res.send(user);
+    }
+  });
 
 });
 
 
 
 router.delete('/:id', (req, res) => {
-    let id = req.params.id;
-    User.remove({_id: id}, (error) => {
-        res.send(error);
-    })
+  let id = req.params.id;
+
+  User.remove({_id: id}, (error) => {
+    res.send(error);
+  })
+
 });
 
 
--- src/users/Form.vue
+++ src/users/Form.vue
@@ -1,26 +1,47 @@
 <template>
-  <form method='post'>
+  <form v-on:submit.prevent='regist'>
     <div>
       <label>name</label>
-      <input type='text'>
+      <input type='text' v-model='user.name'>
     </div>
 
     <div>
       <label>furigana</label>
-      <input type='text'>
+      <input type='text' v-model='user.furigana'>
     </div>
 
     <div>
       <label>mail address</label>
-      <input type='text'>
+      <input type='text' v-model='user.mailAddress'>
     </div>
 
     <button type='submit'>regist</button>
   </form>
+  <pre>{{user|json}}</pre>
 </template>
 
 <script>
+import superagent from 'superagent'
+
 export default {
+  data(){
+    return {
+      user: {},
+    }
+  },
+  methods: {
+    regist(){
+      let user = this.user
+
+      superagent
+        .post('/api/users')
+        .send(user)
+        .end((err, res) => {
+          this.$router.go('.')
+        })
+
+    },
+  },
 }
 </script>
 
